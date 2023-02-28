b087c09c1835373d266cba58f54845a149995dd1
Materonlove <materonlove@gmail.com>
2023-02-28 02:54:38 +0000

    arreglado

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -5,27 +5,46 @@ const SecondsCounter = (props) => {
         <div className="container-fluid d-flex justify-content-center align-items-center mt-5  " id="fondo">
           <div className="row">
             
-          <div className="col-3 text-center">
+          <div className="col-2 text-center">
               <h1>
                 <span className="badge bg-warning"><i className="fa fa-clock"></i></span>
               </h1>
             </div>
-
-            <div className="col-3 text-center">
+            <div className="col-2 text-center">
+              <h1>
+                <span className="badge bg-warning">{props.milSeconds}</span>
+              </h1>
+            </div>
+            <div className="col-2 text-center">
+              <h1>
+                <span className="badge bg-warning">{props.dosSeconds}</span>
+              </h1>
+            </div>
+            <div className="col-2 text-center">
+              <h1>
+                <span className="badge bg-warning">{props.tresSeconds}</span>
+              </h1>
+            </div>
+            <div className="col-2 text-center">
               <h1>
                 <span className="badge bg-warning">{props.centenaSeconds}</span>
               </h1>
             </div>
-            <div className="col-3 text-center">
+            <div className="col-1 text-center">
               <h1>
                 <span className="badge bg-warning">{props.decenaSeconds}</span>
               </h1>
             </div>
-            <div className="col-3 text-center">
+            <div className="col-1 text-center">
               <h1>
                 <span className="badge bg-warning">{props.seconds}</span>
               </h1>
             </div>
+            
+
+
+
+
           </div>
         </div>
       </>
--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,9 @@ import SecondsCounter from "./component/secondsCounter.jsx";
 let tiempoSegundos = 0
 let decenaSeconds = 0
 let centenaSeconds = 0
+let milSeconds = 0
+let dosSeconds = 0
+let tresSeconds = 0
 
 setInterval(() => {
             tiempoSegundos = tiempoSegundos + 1
@@ -24,6 +27,23 @@ setInterval(() => {
                     decenaSeconds = 0
                     centenaSeconds++
                     break;
+
+                case tiempoSegundos > 9:
+                    tiempoSegundos = 0
+                    milSeconds++
+                    break;
+                case decenaSeconds > 5:
+                    decenaSeconds = 0
+                    dosSeconds++
+                    break;
+
+                case decenaSeconds > 5:
+                    decenaSeconds = 0
+                    tresSeconds++
+                    break;
+
+
+
                 default:
                     break
             }
@@ -34,9 +54,22 @@ setInterval(() => {
                 decenaSeconds = {
                     decenaSeconds
                 }
+                milSeconds = {
+                    milSeconds
+                }
+                dosSeconds = {
+                    dosSeconds
+                }
+                tresSeconds = {
+                    tresSeconds
+                }
                 seconds = {
                     tiempoSegundos
                 }
+
+
+                
+
                 />, document.querySelector("#app"));
 
             }, 100)
